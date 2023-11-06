8681a0fab02f223534b62372f4b9e1d5af4d904b
Augusto <Augusto@DESKTOP-DPOCK5N>
2023-11-06 07:52:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorHexas/Program.cs
@@ -0,0 +1,13 @@
+﻿//Declaração de variáveis
+string Hexadecimal, Binario; 
+int Decimal;
+
+Console.WriteLine("Conversor de Hexadecimal para Decimal e Binário");
+Console.Write("Informe o valor a ser convertido (Letras MAIÚSCULAS): ");
+Hexadecimal = Console.ReadLine();   // Hexadecimal recebe o valor a ser convertido
+
+Decimal = Convert.ToInt32(Hexadecimal, 16); //Toint32 converte uma string pra int de base 10 e Decimal recebe esse valor
+Binario = Convert.ToString(Decimal, 2); //ToString converte um int para uma string na base especificada e Binario recebe esse valor
+
+Console.WriteLine($"\n{Hexadecimal} convertido em decimal = {Decimal}");    //Impressão da conversão pra Decimal
+Console.WriteLine($"\n{Hexadecimal} convertido em binario = {Binario}");    //Impressão da conversão pra Binario
---
+++ HexaDecimais/Program.cs
@@ -0,0 +1,29 @@
+﻿//Declaração de variáveis
+
+char[] Alfabeto = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+//Alfabeto, array de caracteres, representa o alfabeto Hexadecimal
+
+string Valor;   //Valor, variável string responsável por armazenar o valor que queremos verificar se é Hexa ou não
+bool Resultado = true;  //Resultado, variável booleana utilizada para validar um If no final do código
+
+Console.WriteLine("Identificador de Hexadecimais");
+Console.Write("Informe um valor (Letras MAIÚSCULAS): ");
+Valor = Console.ReadLine();    //Valor recebe o que for escrito no console
+char[] Comparador = Valor.ToCharArray();    //Converte a string Valor em char e armazena eles no array Comparador
+
+foreach(var C in Comparador)    //Percorrerá cada item (C) do array Comparador
+{
+    if (Alfabeto.Contains(C) == false)  //Verifica se C NÃO está contido no Alfabeto
+    {
+        Console.WriteLine($"O valor {Valor} NÃO é um Hexadecimal");
+        Resultado = false;  //Se C NÃO está contido em Alfabeto, Resultado recebe false. Portanto, Valor NÃO é Hexadecimal
+        break;  //Encerra o loop do foreach
+    }
+
+}
+
+if (Resultado)  //Se Resultado não receber false durante o foreach, Valor é um Hexadecimal
+{
+    Console.WriteLine($"O valor {Valor} é um Hexadecimal");
+}
+
